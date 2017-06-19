524a2a112a0a0f779764065149818b9a1d35db48
John Mount <jmount@win-vector.com>
2017-06-19 03:50:15 -0700

    confirm function mapping

--- tests/testthat/testc.R
+++ tests/testthat/testc.R
@@ -9,11 +9,11 @@ test_that("testc.R", {
   X_ <- 5
 
   let(
-    c(X = 'y'),
+    c(X = 'y', F = 'sin'),
     {
       d <- data.frame("X" = "X", X2 = "XX", d = X*X, .X = X_,
                       stringsAsFactors = FALSE)
-      X <- list(X = d$X, X2 = d$"X", v1 = `X`, v2 = ` X`)
+      X <- list(X = d$X, X2 = d$"X", v1 = `X`, v2 = ` X`, fX = F(1:3))
     })
   expect_equal(y$X2, 'X')
 })
