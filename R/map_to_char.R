cce47fc6bb59f5df7602e69def7f45f6b89ff7e6
John Mount <jmount@win-vector.com>
2017-12-07 08:47:37 -0800

    add separator control to map_to_char

--- R/map_to_char.R
+++ R/map_to_char.R
@@ -3,6 +3,7 @@
 #' format a map.
 #'
 #' @param mp named vector or list
+#' @param sep separator
 #' @return character formatted representation
 #'
 #' @examples
@@ -11,11 +12,11 @@
 #'
 #' @export
 #'
-map_to_char <- function(mp) {
+map_to_char <- function(mp, sep = "\n  ") {
   nms <- names(mp)
   nv <- vapply(nms,
                function(ni) {
                  paste(shQuote(ni), ':=', shQuote(mp[[ni]]))
                }, character(1))
-  paste0("c(", paste(nv, collapse = ", "), ")")
+  paste0("c(", paste(nv, collapse = paste0(",", sep)), ")")
 }
