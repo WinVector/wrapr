411adde8ecd2801bb00edfd6558578c66c7e5790
John Mount <jmount@win-vector.com>
2018-02-19 12:19:06 -0800

    add stop_if_dots_args()

---
+++ R/stop_if_dot_args.R
@@ -0,0 +1,39 @@
+
+#' Stop with message if dot_args is a non-trivial list.
+#'
+#' Generate a stop with a good error message if the dots argument was a non-trivial list.
+#' Useful in writing functions that force named arguments.
+#'
+#' @param dot_args substitute(list(...)) from another function.
+#' @param msg character, optional message to prepend.
+#' @return NULL or stop()
+#'
+#' @examples
+#'
+#' f <- function(x, ..., inc = 1) {
+#'    wrapr::stop_if_dot_args(substitute(list(...)), "f")
+#'    x + inc
+#' }
+#' f(7)
+#' f(7, inc = 2)
+#' tryCatch(
+#'   f(7, 2),
+#'   error = function(e) { print(e) }
+#' )
+#'
+#' @export
+#'
+stop_if_dot_args <- function(dot_args, msg = "") {
+  if(length(dot_args)>1) {
+    unams <- names(dot_args)[-1]
+    uvals <- as.character(dot_args)[-1]
+    unams[is.null(unams)] <- ""
+    not_null <- nchar(unams)>0
+    unams[not_null] <- paste0(unams[not_null], " = ")
+    unams[!not_null] <- ""
+    str <- paste(paste0(unams, uvals), collapse = ", ")
+    stop(paste(msg, "unexpected arguments:", str),
+         call. = FALSE)
+  }
+  NULL
+}
