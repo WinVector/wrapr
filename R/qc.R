3540568eec83ee157d39609c27815975c62b8688
John Mount <jmount@win-vector.com>
2018-05-20 14:12:54 -0700

    fix qc() to quote more often

--- R/qc.R
+++ R/qc.R
@@ -2,6 +2,13 @@
 
 #' Quoting version of c() array concatinator.
 #'
+#' The qc() function is intended to help quote user inputs.
+#' It is a convenience function allowing the user to elide
+#' excess quotation marks.  It quotes its arguments instead
+#' of evaluating them, except in the case of a nested
+#' call to qc() itself.  Please see the examples for
+#' typical uses both for named and un-named character vectors.
+#'
 #'
 #' @param ... items to place into an array
 #' @return quoted array of character items
@@ -10,9 +17,18 @@
 #'
 #' @examples
 #'
-#' qc(a, qc(b, c))
-#' qc(x=a, qc(y=b, z=c))
-#' qc('x'='a', qc('y'='b', 'z'='c'))
+#' a <- "x"
+#' qc(a) # returns the string "a" (not "x")
+#'
+#' qc("a") # return the string "a" (not "\"a\"")
+#'
+#' qc(sin(x))  # returns the string "sin(x)"
+#'
+#' qc(a, qc(b, c)) # returns c("a", "b", "c")
+#'
+#' qc(x=a, qc(y=b, z=c)) # returns c(x="a", y="b", z="c")
+#'
+#' qc('x'='a', qc('y'='b', 'z'='c')) # returns c(x="a", y="b", z="c")
 #'
 #' @export
 #'
@@ -23,39 +39,49 @@ qc <- function(...) {
   }
   .wrapr_priveate_var_env <- parent.frame()
   .wrapr_priveate_var_names <- names(.wrapr_priveate_var_args)
-  .wrapr_priveate_var_res <- lapply(2:length(.wrapr_priveate_var_args),
-                function(.wrapr_priveate_var_i) {
-                  .wrapr_priveate_var_ei <- .wrapr_priveate_var_args[[.wrapr_priveate_var_i]]
-                  .wrapr_priveate_var_ni <- NULL
-                  if(.wrapr_priveate_var_i<=length(.wrapr_priveate_var_names)) {
-                    .wrapr_priveate_var_ni <- as.character(.wrapr_priveate_var_names[[.wrapr_priveate_var_i]])
-                  }
-                  if(is.name(.wrapr_priveate_var_ei) | is.character(.wrapr_priveate_var_ei)) {
-                    if(is.null(.wrapr_priveate_var_ni)) {
-                      return(as.character(.wrapr_priveate_var_ei))
-                    } else {
-                      return(.wrapr_priveate_var_ni := as.character(.wrapr_priveate_var_ei))
-                    }
-                  }
-                  .wrapr_priveate_var_ln = length(.wrapr_priveate_var_ei)
-                  if(.wrapr_priveate_var_ln<=0) {
-                    return(NULL)
-                  }
-                  if(.wrapr_priveate_var_ln<=1) {
-                    if(is.null(.wrapr_priveate_var_ni)) {
-                      return(as.character(.wrapr_priveate_var_ei))
-                    } else {
-                      return(.wrapr_priveate_var_ni := as.character(.wrapr_priveate_var_ei))
-                    }
-                  }
-                  # complex structure, like a list
-                  if(is.language(.wrapr_priveate_var_ei)) {
-                    return(eval(.wrapr_priveate_var_ei,
-                                envir = .wrapr_priveate_var_env,
-                                enclos = .wrapr_priveate_var_env))
-                  }
-                  return(.wrapr_priveate_var_ei[2:.wrapr_priveate_var_ln])
-                })
+  .wrapr_priveate_var_res <- lapply(
+    2:length(.wrapr_priveate_var_args),
+    function(.wrapr_priveate_var_i) {
+      .wrapr_priveate_var_ei <- .wrapr_priveate_var_args[[.wrapr_priveate_var_i]]
+      .wrapr_priveate_var_ni <- NULL
+      if(.wrapr_priveate_var_i<=length(.wrapr_priveate_var_names)) {
+        .wrapr_priveate_var_ni <- .wrapr_priveate_var_names[[.wrapr_priveate_var_i]]
+      }
+      if(is.name(.wrapr_priveate_var_ei)) {
+        # names are scalars
+        .wrapr_priveate_var_ei <- as.character(.wrapr_priveate_var_ei)
+        if(!is.null(.wrapr_priveate_var_ni)) {
+          names(.wrapr_priveate_var_ei) <- .wrapr_priveate_var_ni
+        }
+        return(.wrapr_priveate_var_ei)
+      }
+      if(is.language(.wrapr_priveate_var_ei)) {
+        if(isTRUE(as.character(.wrapr_priveate_var_ei[[1]]) == "qc")) {
+          # this is the recursive case qc('x'='a', qc('y'='b', 'z'='c'))
+          .wrapr_priveate_var_ei <- eval(.wrapr_priveate_var_ei,
+                                         envir = .wrapr_priveate_var_env,
+                                         enclos = .wrapr_priveate_var_env)
+          return(.wrapr_priveate_var_ei)
+        }
+        # other case: quote expression
+        .wrapr_priveate_var_ei <- paste(deparse(.wrapr_priveate_var_ei), collapse = "\n")
+        if(!is.null(.wrapr_priveate_var_ni)) {
+          names(.wrapr_priveate_var_ei) <- .wrapr_priveate_var_ni
+        }
+        return(.wrapr_priveate_var_ei)
+      }
+      if(is.vector(.wrapr_priveate_var_ei) || is.list(.wrapr_priveate_var_ei)) {
+        if(length(.wrapr_priveate_var_ei)<=0) {
+          return(NULL)
+        }
+      }
+      # base case, character vectors, list, and objects
+      .wrapr_priveate_var_ei <- paste(as.character(.wrapr_priveate_var_ei), collapse = " ")
+      if(!is.null(.wrapr_priveate_var_ni)) {
+        names(.wrapr_priveate_var_ei) <- .wrapr_priveate_var_ni
+      }
+      return(.wrapr_priveate_var_ei)
+    })
   .wrapr_priveate_var_res <- Filter(function(ei) { !is.null(ei) }, .wrapr_priveate_var_res)
   if(length(.wrapr_priveate_var_res)<1) {
     return(c())
