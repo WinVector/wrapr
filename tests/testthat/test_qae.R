c4253d29ea531c9f20a332ac0c4a81b2a5a24a50
John Mount <jmount@win-vector.com>
2018-06-05 17:02:12 -0700

    add qae test

---
+++ tests/testthat/test_qae.R
@@ -0,0 +1,10 @@
+
+context("qae")
+
+test_that("test_qae.R", {
+  v <- wrapr::qae(a = 1, b := 2, c %:=% 3)
+  expect_equal(list(a = "1", b = "2", c = "3"), v)
+  library("wrapr")
+  v2 <- qae(a = 1, b := 2, c %:=% 3)
+  expect_equal(list(a = "1", b = "2", c = "3"), v2)
+})
