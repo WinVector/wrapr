e4570510b54e6c3e8661b719296b78922f58f862
John Mount <jmount@win-vector.com>
2018-09-08 12:15:36 -0700

    Add sortv()

--- R/orderv.R
+++ R/orderv.R
@@ -11,7 +11,7 @@
 #' @return ordering permutation
 #'
 #'
-#' @seealso \code{\link[base]{order}}
+#' @seealso \code{\link[base]{order}}, \code{\link{sortv}}
 #'
 #' @examples
 #'
@@ -36,4 +36,49 @@ orderv <- function(columns,
       na.last = na.last,
       decreasing = decreasing,
       method = method)))
+}
+
+#' Sort a data.frame.
+#'
+#' Sort a data.frame by a set of columns.
+#'
+#' @param data data.frame to sort.
+#' @param colnames column names to sort on.
+#' @param ... not used, force later arguments to bind by name.
+#' @param na.last (passed to \code{\link[base]{order}}) for controlling the treatment of NAs. If TRUE, missing values in the data are put last; if FALSE, they are put first; if NA, they are removed.
+#' @param decreasing (passed to \code{\link[base]{order}}) logical. Should the sort order be increasing or decreasing? For the "radix" method, this can be a vector of length equal to the number of arguments in \code{...}. For the other methods, it must be length one.
+#' @param method (passed to \code{\link[base]{order}}) the method to be used: partial matches are allowed. The default ("auto") implies "radix" for short numeric vectors, integer vectors, logical vectors and factors. Otherwise, it implies "shell". For details of methods "shell", "quick", and "radix", see the help for \code{\link[base]{sort}}.
+#' @return ordering permutation
+#'
+#'
+#' @seealso \code{\link{orderv}}
+#'
+#' @examples
+#'
+#' d <- data.frame(x = c(2, 2, 3, 3, 1, 1), y = 6:1)
+#' sortv(d, c("x", "y"))
+#'
+#' @export
+#'
+sortv <- function(data,
+                  colnames,
+                  ...,
+                  na.last = TRUE,
+                  decreasing = FALSE,
+                  method = c("auto", "shell", "radix")) {
+  wrapr::stop_if_dot_args(substitute(list(...)), "wrapr::sortv")
+  if(!is.data.frame(data)) {
+    stop("wrapr::sortv data must be a data.frame")
+  }
+  if(!is.character(colnames)) {
+    stop("wrapr::sortv dcolnamesata must be of type character")
+  }
+  if(length(colnames)<1) {
+    return(data)
+  }
+  perm <- orderv(as.list(data[, colnames, drop= FALSE]),
+                 na.last = na.last,
+                 decreasing = decreasing,
+                 method = method)
+  data[perm, , drop = FALSE]
 }
