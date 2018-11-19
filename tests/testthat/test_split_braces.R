4112002c3e152e5ebb8787fd2ea655bce4e38940
John Mount <jmount@win-vector.com>
2018-11-19 12:00:32 -0800

    test list cases

--- tests/testthat/test_split_braces.R
+++ tests/testthat/test_split_braces.R
@@ -10,6 +10,10 @@ test_that("test_split_braces.R", {
   testthat::expect_equal(split_at_brace_pairs("{x} + y + {z}"),
                          c("{x}", " + y + ", "{z}"))
 
+  testthat::expect_equal(split_at_brace_pairs(list("{x} + y + {z}")),
+                         list(c("{x}", " + y + ", "{z}")))
+
+
   testthat::expect_equal(split_at_brace_pairs("x + {y} + z"),
                          c("x + ", "{y}", " + z"))
 
@@ -21,7 +25,12 @@ test_that("test_split_braces.R", {
 
   testthat::expect_equal(split_at_brace_pairs(c("{x} + y",
                                                 "{x} + y + {z}")),
-                                              list(c("{x}", " + y"),
-                                                   c("{x}", " + y + ", "{z}")))
+                         list(c("{x}", " + y"),
+                              c("{x}", " + y + ", "{z}")))
+
+  testthat::expect_equal(split_at_brace_pairs(list("{x} + y",
+                                                   "{x} + y + {z}")),
+                         list(c("{x}", " + y"),
+                              c("{x}", " + y + ", "{z}")))
 
 })
