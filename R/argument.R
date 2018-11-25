62cd1eb67169d89440b469674bf31e73bebfd919
John Mount <jmount@win-vector.com>
2018-11-25 11:29:21 -0800

    harden as_value code and switch to S3 methods

--- R/argument.R
+++ R/argument.R
@@ -10,12 +10,35 @@
 #' @export
 #'
 as_argument <- function(x) {
-  if("Argument" %in% class(x)) {
-    return(x)
-  }
+  UseMethod("as_argument", x)
+}
+
+#' Argument constructor
+#'
+#' Build an Argument class from an object.
+#'
+#' @param x object to wrap
+#' @return x wrapped in an Argument container.
+#'
+#' @export
+#'
+as_argument.default <- function(x) {
   r <- list(x)
   class(r) <- "Argument"
   r
+}
+
+#' Argument constructor
+#'
+#' Pass through unaltered.
+#'
+#' @param x object to wrap
+#' @return x wrapped in an Argument container.
+#'
+#' @export
+#'
+as_argument.Argument <- function(x) {
+  x
 }
 
 #' Convert from Argument to value.
@@ -66,6 +89,10 @@ as_value.Argument <- function(x) {
 #' @param right_arg_name name, if not NULL name of right argument.
 #' @return result
 #'
+#' @examples
+#'
+#' as_argument(1:3) %.>% wrapfn(sin, "x") %.>% wrapfn(cos, "x") %.>% as_value(.)
+#'
 #' @export
 #'
 apply_left.Argument <-  function(pipe_left_arg,
@@ -76,6 +103,14 @@ apply_left.Argument <-  function(pipe_left_arg,
                                  right_arg_name) {
   force(pipe_environment)
   pipe_left_arg <- as_value(pipe_left_arg)
+  if(is.call(pipe_right_arg) && (as.character(pipe_right_arg[[1]])=="as_value")) {
+    return(apply_left(pipe_left_arg = pipe_left_arg,
+                      pipe_right_arg = pipe_right_arg,
+                      pipe_environment = pipe_environment,
+                      left_arg_name = left_arg_name,
+                      pipe_string = pipe_string,
+                      right_arg_name = right_arg_name))
+  }
   pipe_right_arg <- eval(pipe_right_arg,
                          envir = pipe_environment,
                          enclos = pipe_environment)
