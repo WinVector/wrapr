fae5005da57def9cc6f2d2aa03d76f8cde32a12e
John Mount <jmount@win-vector.com>
2019-02-03 11:28:57 -0800

    update testing notes

--- R/run_package_tests.R
+++ R/run_package_tests.R
@@ -8,7 +8,8 @@
 #' Attaches RUnit and pkg, requires RUnit.
 #' Stops on error.
 #'
-#' Based on \url{https://github.com/RcppCore/Rcpp/blob/master/tests/doRUnit.R}.
+#' Based on \url{https://github.com/RcppCore/Rcpp/blob/master/tests/doRUnit.R}.  This
+#' version is GPL-3, works derived from it must be distributed GPL-3.
 #'
 #' @param pkg character, name of package to test.
 #' @param ... not used, force later arguments to bind by name.
