e19ac5a4b9bd234fbab40f4c43510aa5200e7f81
John Mount <jmount@win-vector.com>
2020-01-20 15:18:37 -0800

    switch to implicit naming

--- R/unpack.R
+++ R/unpack.R
@@ -1,9 +1,7 @@
 
-# get character argument names out of ... with bquote .() evaluaiton in wrapr_private_unpack_environment
-capture_and_validate_assignment_targets <- function(wrapr_private_unpack_environment, ...) {
-  force(wrapr_private_unpack_environment)
-  # capture the arguments unevaluted, and run through bquote
-  args <- as.list(do.call(bquote, list(substitute(list(...)), where = wrapr_private_unpack_environment)))[-1]
+# validate source and target naming
+validate_assignment_targets <- function(args,
+                                        allow_unnamed_case) {
   # extract and validate arguments as names of unpack targets
   nargs <- length(args)
   if(nargs < 1) {
@@ -73,21 +71,25 @@ capture_and_validate_assignment_targets <- function(wrapr_private_unpack_environ
     names(values) <- str_targets
     str_targets <- values
   }
+  # promote up to named case
+  if((length(names(str_targets)) <= 0) && (!allow_unnamed_case)) {
+    names(str_targets) <- str_targets
+  }
   return(str_targets)
 }
 
 
 #' write values into environment
 #'
-#' @param wrapr_private_unpack_environment environment to write into
+#' @param unpack_environment environment to write into
 #' @param str_args array of string-names to write to (either without names, or names as targets)
 #' @param value values to write
 #'
 #' @keywords internal
 #' @noRd
 #'
-write_values_into_env <- function(wrapr_private_unpack_environment, str_args, value) {
-  force(wrapr_private_unpack_environment)
+write_values_into_env <- function(unpack_environment, str_args, value) {
+  force(unpack_environment)
   nargs <- length(str_args)
   named_case <- length(names(str_args)) > 0
   if(named_case) {
@@ -113,7 +115,7 @@ write_values_into_env <- function(wrapr_private_unpack_environment, str_args, va
       if(!is.na(source_i)) {
         value_i <-value[[source_i]]
       }
-      assign(x = dest_i, value = value_i, envir = wrapr_private_unpack_environment)
+      assign(x = dest_i, value = value_i, envir = unpack_environment)
     }
   } else {
     nvalue <- length(value)
@@ -130,7 +132,7 @@ write_values_into_env <- function(wrapr_private_unpack_environment, str_args, va
       source_i <- i
       value_i <- value[[source_i]]
       if(!is.na(dest_i)) {
-        assign(x = dest_i, value = value_i, envir = wrapr_private_unpack_environment)
+        assign(x = dest_i, value = value_i, envir = unpack_environment)
       }
     }
   }
@@ -146,53 +148,63 @@ write_values_into_env <- function(wrapr_private_unpack_environment, str_args, va
 #' as a side-effect.
 #'
 #' @param object_name character, name the object is stored as
+#' @param allow_unnamed_case logical, if FALSE promote unnamed arguments to self-named
 #' @return an unpacker
 #'
 #' @keywords internal
 #' @export
 #'
-define_unpacker <- function(object_name) {
+Unpacker <- function(object_name = NULL, allow_unnamed_case = FALSE) {
   force(object_name)
+  force(allow_unnamed_case)
   if(!is.null(object_name)) {
     if(!is.character(object_name)) {
-      stop("wrapr::define_unpacker object_name must be character when not NULL")
+      stop("wrapr::Unpacker object_name must be character when not NULL")
     }
     if(length(object_name) != 1) {
-      stop("wrapr::define_unpacker object_naem must be length 1 when not NULL")
+      stop("wrapr::Unpacker object_naem must be length 1 when not NULL")
     }
   }
 
   f <- function(wrapr_private_value, ...) {
     # get environment to work in
-    wrapr_private_unpack_environment <- parent.frame(n = 1)
+    unpack_environment <- parent.frame(n = 1)
     # get the targets
-    str_args <- capture_and_validate_assignment_targets(wrapr_private_unpack_environment, ...)
+    # capture the arguments unevaluted, and run through bquote
+    str_args <- as.list(do.call(bquote, list(substitute(list(...)), where = unpack_environment)))[-1]
+    str_args <- validate_assignment_targets(args = str_args, allow_unnamed_case = allow_unnamed_case)
     force(wrapr_private_value)
-    write_values_into_env(wrapr_private_unpack_environment = wrapr_private_unpack_environment, str_args = str_args, value = wrapr_private_value)
+    write_values_into_env(unpack_environment = unpack_environment, str_args = str_args, value = wrapr_private_value)
     invisible(wrapr_private_value)
   }
 
   attr(f, 'object_name') <- object_name
-  class(f) <- 'unpacker'
+  attr(f, 'allow_unnamed_case') <- allow_unnamed_case
+  class(f) <- "Unpacker"
   return(f)
 }
 
 
 #' @export
-format.unpacker <- function(x, ...) {
+format.Unpacker <- function(x, ...) {
   object_name <- attr(x, 'object_name')
-  return(object_name)
+  allow_unnamed_case <- attr(x, 'allow_unnamed_case')
+  q_name <- "NULL"
+  if(!is.null(object_name)) {
+    q_name <- sQuote(object_name)
+  }
+  return(paste0("wrapr::Unpacker(object_name = ", q_name, ", allow_unnamed_case = ", allow_unnamed_case, ")"))
 }
 
 
 #' @export
-as.character.unpacker <- function(x, ...) {
+as.character.Unpacker <- function(x, ...) {
   format(x, ...)
 }
 
 
 #' @export
-print.unpacker <- function(x, ...) {
+print.Unpacker <- function(x, ...) {
   cat(format(x, ...))
 }
 
@@ -217,36 +229,32 @@ print.unpacker <- function(x, ...) {
 #' @examples
 #'
 #' # named unpacking
-#' # looks like assignment: destination = where_from_in_value
+#' # looks like assignment: DESTINATION = NAME_VALUE_USING
 #' d <- data.frame(x = 1:2,
 #'                 g=c('test', 'train'),
 #'                 stringsAsFactors = FALSE)
-#' into[train_set = train, test_set = test] <- split(d, d$g)
+#' to[train_set = train, test_set = test] <- split(d, d$g)
 #' # train_set and test_set now correctly split
 #' print(train_set)
 #' print(test_set)
+#' rm(list = c('train_set', 'test_set'))
 #'
-#' # name capture version
-#' into[a, b] <- list(5, 10)
-#' print(a)  # now 5
-#' print(b)  # now 10
+#' # named unpacking NEWNAME = OLDNAME implicit form
+#' # values are matched by name, not index
+#' to[train, test] <- split(d, d$g)
+#' print(train)
+#' print(test)
+#' rm(list = c('train', 'test'))
 #'
-#' # bquote re-direct to value in variable using .()
-#' # Note: the bquote .() step is potentially confusing, as the user
-#' # can't immediately see where the value is being assigned to.
-#' # Also, quotes are allowed.
-#' a <- 'x'
-#' into[.(a), 'b'] <- list(20, 40)
-#' print(x)  # now 20
-#' print(b)  # now 40
-#' print(a)  # still 'x'
 #'
 #' @export
 #'
-`[<-.unpacker` <- function(wrapr_private_self, ..., value) {
+`[<-.Unpacker` <- function(wrapr_private_self, ..., value) {
   force(wrapr_private_self)
   # get environment to work in
-  wrapr_private_unpack_environment <- parent.frame(n = 1)
+  unpack_environment <- parent.frame(n = 1)
+  # capture .. args
+  str_args <- as.list(do.call(bquote, list(substitute(list(...)), where = unpack_environment)))[-1]
   # the array update is going to write an object into the
   # destination environment after returning from this method,
   # try to ensure it is obvious it is the exact
@@ -256,15 +264,19 @@ print.unpacker <- function(x, ...) {
   object_name <- attr(wrapr_private_self, 'object_name')
   if(!is.null(object_name)) {
     old_value <- base::mget(object_name,
-                            envir = wrapr_private_unpack_environment,
+                            envir = unpack_environment,
                             mode = "any",
-                            ifnotfound = list(wrapr_private_self),
+                            ifnotfound = list(NULL),
                             inherits = FALSE)[[1]]
-    if(!("unpacker" %in% class(old_value))) {
-      stop(paste0("running upacker would overwrite ", object_name, " which is not an unpacker"))
+    if(!is.null(old_value)) {
+      if(!("Unpacker" %in% class(old_value))) {
+        stop(paste0("running upacker would overwrite ", object_name, " which is not an unpacker"))
+      }
     }
+    old_value <- NULL
   }
-  str_args <- capture_and_validate_assignment_targets(wrapr_private_unpack_environment, ...)
+  allow_unnamed_case <- isTRUE(attr(wrapr_private_self, 'allow_unnamed_case'))
+  str_args <- validate_assignment_targets(args = str_args, allow_unnamed_case = allow_unnamed_case)
   if(!is.null(object_name)) {
     named_case <- length(names(str_args)) > 0
     if(named_case) {
@@ -284,14 +296,14 @@ print.unpacker <- function(x, ...) {
     }
   }
   force(value)
-  write_values_into_env(wrapr_private_unpack_environment = wrapr_private_unpack_environment, str_args = str_args, value = value)
+  write_values_into_env(unpack_environment = unpack_environment, str_args = str_args, value = value)
   # the return value gets written into executing environment after return
   # R expects this to be wrapr_private_self, so do that instead of returning old_value
   return(wrapr_private_self)
 }
 
 
-#' Unpack or bind values into the calling environment.
+#' Unpack or bind values by names into the calling environment.
 #'
 #' Unpacks or binds values into the calling environment. Uses \code{bquote} escaping.
 #' NULL is a special case that is unpacked to all targets. NA targets are skipped.
@@ -310,7 +322,7 @@ print.unpacker <- function(x, ...) {
 #' @examples
 #'
 #' # named unpacking
-#' # looks like assignment: destination = where_from_in_value
+#' # looks like assignment: DESTINATION = NAME_VALUE_USING
 #' d <- data.frame(x = 1:2,
 #'                 g=c('test', 'train'),
 #'                 stringsAsFactors = FALSE)
@@ -318,35 +330,36 @@ print.unpacker <- function(x, ...) {
 #' # train_set and test_set now correctly split
 #' print(train_set)
 #' print(test_set)
+#' rm(list = c('train_set', 'test_set'))
 #'
-#' # name capture version
-#' into[a, b] <- list(5, 10)
-#' print(a)  # now 5
-#' print(b)  # now 10
+#' # named unpacking NEWNAME = OLDNAME implicit form
+#' # values are matched by name, not index
+#' into[train, test] <- split(d, d$g)
+#' print(train)
+#' print(test)
+#' rm(list = c('train', 'test'))
 #'
-#' # bquote re-direct to value in variable using .()
-#' # Note: the bquote .() step is potentially confusing, as the user
-#' # can't immediately see where the value is being assigned to.
-#' # Also, quotes are allowed.
-#' a <- 'x'
-#' into[.(a), 'b'] <- list(20, 40)
-#' print(x)  # now 20
-#' print(b)  # now 40
-#' print(a)  # still 'x'
+#' # function version
+#' into(split(d, d$g), train, test)
+#' print(train)
+#' print(test)
+#' rm(list = c('train', 'test'))
 #'
 #' # pipe version
-#' list(55, 15) %.>% into(., a, b)
-#' print(a)  # now 55
-#' print(b)  # now 15
-#' # Note: the above example will not work with magrittr pipe,
-#' # as in that case the values get written to an intermediate
-#' # environment and lost.
+#' split(d, d$g) %.>% into(., train, test)
+#' print(train)
+#' print(test)
+#' rm(list = c('train', 'test'))
+#' # Note: above is wrapr dot-pipe, piping does not currently work with
+#' # magrittr pipe due to magrittr's introduction of temporary
+#' # intermediate environments during evaluation.
+#'
 #'
 #' @export
 #'
-into <- define_unpacker("into")
+into <- Unpacker(object_name = "into", allow_unnamed_case = FALSE)
 
-#' Unpack or bind values into the calling environment.
+#' Unpack or bind values by names into the calling environment.
 #'
 #' Unpacks or binds values into the calling environment. Uses \code{bquote} escaping.
 #' NULL is a special case that is unpacked to all targets. NA targets are skipped.
@@ -365,7 +378,7 @@ into <- define_unpacker("into")
 #' @examples
 #'
 #' # named unpacking
-#' # looks like assignment: destination = where_from_in_value
+#' # looks like assignment: DESTINATION = NAME_VALUE_USING
 #' d <- data.frame(x = 1:2,
 #'                 g=c('test', 'train'),
 #'                 stringsAsFactors = FALSE)
@@ -373,35 +386,90 @@ into <- define_unpacker("into")
 #' # train_set and test_set now correctly split
 #' print(train_set)
 #' print(test_set)
+#' rm(list = c('train_set', 'test_set'))
 #'
-#' # name capture version
-#' to[a, b] <- list(5, 10)
-#' print(a)  # now 5
-#' print(b)  # now 10
+#' # named unpacking NEWNAME = OLDNAME implicit form
+#' # values are matched by name, not index
+#' to[train, test] <- split(d, d$g)
+#' print(train)
+#' print(test)
+#' rm(list = c('train', 'test'))
 #'
-#' # bquote re-direct to value in variable using .()
-#' # Note: the bquote .() step is potentially confusing, as the user
-#' # can't immediately see where the value is being assigned to.
-#' # Also, quotes are allowed.
-#' a <- 'x'
-#' to[.(a), 'b'] <- list(20, 40)
-#' print(x)  # now 20
-#' print(b)  # now 40
-#' print(a)  # still 'x'
+#' # function version
+#' to(split(d, d$g), train, test)
+#' print(train)
+#' print(test)
+#' rm(list = c('train', 'test'))
 #'
 #' # pipe version
-#' list(55, 15) %.>% to(., a, b)
-#' print(a)  # now 55
-#' print(b)  # now 15
-#' # Note: the above example will not work with magrittr pipe,
-#' # as in that case the values get written to an intermediate
-#' # environment and lost.
+#' split(d, d$g) %.>% to(., train, test)
+#' print(train)
+#' print(test)
+#' rm(list = c('train', 'test'))
+#' # Note: above is wrapr dot-pipe, piping does not currently work with
+#' # magrittr pipe due to magrittr's introduction of temporary
+#' # intermediate environments during evaluation.
 #'
 #' @export
 #'
-to <- define_unpacker("to")
+to <- Unpacker(object_name = "to", allow_unnamed_case = FALSE)
 
-#' Unpack or bind values into the calling environment.
+#' Unpack or bind values by names into the calling environment.
+#'
+#' Unpacks or binds values into the calling environment. Uses \code{bquote} escaping.
+#' NULL is a special case that is unpacked to all targets. NA targets are skipped.
+#' All non-NA target names must be unique.
+#'
+#' Similar to \code{Python} tuple unpacking, \code{zeallot}'s arrow, and to \code{vadr::bind}.
+#'
+#' Note: a reference to the unpacker object is written into the unpacking environment as a side-effect
+#' of the implied array assignment.
+#' Array-assign form can not use the names: \code{.}, \code{wrapr_private_self}, \code{value}, or \code{unpack}.
+#' Function form can not use the names: \code{.} or \code{wrapr_private_value}.
+#'
+#' @param wrapr_private_value list of values to copy
+#' @param ... argument names to write to
+#'
+#' @examples
+#'
+#' # named unpacking
+#' # looks like assignment: DESTINATION = NAME_VALUE_USING
+#' d <- data.frame(x = 1:2,
+#'                 g=c('test', 'train'),
+#'                 stringsAsFactors = FALSE)
+#' unpack[train_set = train, test_set = test] <- split(d, d$g)
+#' # train_set and test_set now correctly split
+#' print(train_set)
+#' print(test_set)
+#' rm(list = c('train_set', 'test_set'))
+#'
+#' # named unpacking NEWNAME = OLDNAME implicit form
+#' # values are matched by name, not index
+#' unpack[train, test] <- split(d, d$g)
+#' print(train)
+#' print(test)
+#' rm(list = c('train', 'test'))
+#'
+#' # function version
+#' unpack(split(d, d$g), train, test)
+#' print(train)
+#' print(test)
+#' rm(list = c('train', 'test'))
+#'
+#' # pipe version
+#' split(d, d$g) %.>% unpack(., train, test)
+#' print(train)
+#' print(test)
+#' rm(list = c('train', 'test'))
+#' # Note: above is wrapr dot-pipe, piping does not currently work with
+#' # magrittr pipe due to magrittr's introduction of temporary
+#' # intermediate environments during evaluation.
+#'
+#' @export
+#'
+unpack <- Unpacker(object_name = "unpack", allow_unnamed_case = FALSE)
+
+#' Unpack or bind values by index or names into the calling environment.
 #'
 #' Unpacks or binds values into the calling environment. Uses \code{bquote} escaping.
 #' NULL is a special case that is unpacked to all targets. NA targets are skipped.
@@ -424,28 +492,33 @@ to <- define_unpacker("to")
 #' d <- data.frame(x = 1:2,
 #'                 g=c('test', 'train'),
 #'                 stringsAsFactors = FALSE)
-#' unpack[train_set = train, test_set = test] <- split(d, d$g)
+#' unpack_i[train_set = train, test_set = test] <- split(d, d$g)
 #' # train_set and test_set now correctly split
 #' print(train_set)
 #' print(test_set)
 #'
-#' # name capture version
-#' unpack[a, b] <- list(5, 10)
+#' # un-named version
+#' # values matchced by index
+#' unpack_i[a, b] <- list(5, 10)
 #' print(a)  # now 5
 #' print(b)  # now 10
 #'
+#' # un-named version
+#' #' # values matchced by index
 #' # bquote re-direct to value in variable using .()
 #' # Note: the bquote .() step is potentially confusing, as the user
 #' # can't immediately see where the value is being assigned to.
 #' # Also, quotes are allowed.
 #' a <- 'x'
-#' unpack[.(a), 'b'] <- list(20, 40)
+#' unpack_i[.(a), 'b'] <- list(20, 40)
 #' print(x)  # now 20
 #' print(b)  # now 40
 #' print(a)  # still 'x'
 #'
-#' # pipe version
-#' list(55, 15) %.>% unpack(., a, b)
+#' # un-named version
+#' #' # values matchced by index
+#' # piped example
+#' list(55, 15) %.>% unpack_i(., a, b)
 #' print(a)  # now 55
 #' print(b)  # now 15
 #' # Note: the above example will not work with magrittr pipe,
@@ -454,5 +527,5 @@ to <- define_unpacker("to")
 #'
 #' @export
 #'
-unpack <- define_unpacker("unpack")
+unpack_i <- Unpacker(object_name = "unpack", allow_unnamed_case = TRUE)
 
--- inst/unit_tests/test_unpack.R
+++ inst/unit_tests/test_unpack.R
@@ -1,33 +1,174 @@
-test_unpack <- function() {
+
+test_unpack_unpack_i <- function() {
 
   d <- data.frame(x = 1:2, g=c('test', 'train'))
-  split(d, d$g) -> into[test_set = test, train_set = train]
+  split(d, d$g) -> unpack_i[test_set = test, train_set = train]
   RUnit::checkTrue(test_set$g[[1]] == 'test')
   RUnit::checkTrue(train_set$g[[1]] == 'train')
+  rm(list = c('test_set', 'train_set'))
+
+  # again with self in local environment
+  d <- data.frame(x = 1:2, g=c('test', 'train'))
+  split(d, d$g) -> unpack_i[test_set = test, train_set = train]
+  RUnit::checkTrue(test_set$g[[1]] == 'test')
+  RUnit::checkTrue(train_set$g[[1]] == 'train')
+  rm(list = c('test_set', 'train_set'))
+
 
   a <- 'x'
   # name capture version
-  into[a, b] <- list(5, 10)
+  unpack_i[a, b] <- list(5, 10)
   RUnit::checkEquals(a, 5)
   RUnit::checkEquals(b, 10)
-
-  # into is now in local namespace, 2nd check is also checking that still works
 
   # bquote re-direct to value in variable
   # plus quotes are allowed
   a <- 'x'
-  into[.(a), 'b'] <- list(20, 40)
+  unpack_i[.(a), 'b'] <- list(20, 40)
   RUnit::checkEquals(a, 'x')
   RUnit::checkEquals(x, 20)
   RUnit::checkEquals(b, 40)
 
-  list(256, 2106) %.>% into(., a, b)
+  list(256, 2106) %.>% unpack_i(., a, b)
   RUnit::checkEquals(a, 256)
   RUnit::checkEquals(b, 2106)
 
-  list(2567, 21067) %.>% wrapr::unpack(., a, b)
+  list(2567, 21067) %.>% wrapr::unpack_i(., a, b)
   RUnit::checkEquals(a, 2567)
   RUnit::checkEquals(b, 21067)
 
   invisible(NULL)
+}
+
+test_unpack_unpack <- function() {
+  # named unpacking
+  # looks like assignment: DESTINATION = NAME_VALUE_USING
+  d <- data.frame(x = 1:2,
+                  g=c('test', 'train'),
+                  stringsAsFactors = FALSE)
+  unpack[train_set = train, test_set = test] <- split(d, d$g)
+  # train_set and test_set now correctly split
+  RUnit::checkTrue(test_set$g[[1]] == 'test')
+  RUnit::checkTrue(train_set$g[[1]] == 'train')
+  rm(list = c('train_set', 'test_set'))
+
+  # again with self in local environment
+  # named unpacking
+  # looks like assignment: DESTINATION = NAME_VALUE_USING
+  d <- data.frame(x = 1:2,
+                  g=c('test', 'train'),
+                  stringsAsFactors = FALSE)
+  unpack[train_set = train, test_set = test] <- split(d, d$g)
+  # train_set and test_set now correctly split
+  RUnit::checkTrue(test_set$g[[1]] == 'test')
+  RUnit::checkTrue(train_set$g[[1]] == 'train')
+  rm(list = c('train_set', 'test_set'))
+
+  # named unpacking NEWNAME = OLDNAME implicit form
+  # values are matched by name, not index
+  unpack[train, test] <- split(d, d$g)
+  RUnit::checkTrue(test$g[[1]] == 'test')
+  RUnit::checkTrue(train$g[[1]] == 'train')
+  rm(list = c('train', 'test'))
+
+  # function version
+  unpack(split(d, d$g), train, test)
+  RUnit::checkTrue(test$g[[1]] == 'test')
+  RUnit::checkTrue(train$g[[1]] == 'train')
+  rm(list = c('train', 'test'))
+
+  # pipe version
+  split(d, d$g) %.>% unpack(., train, test)
+  RUnit::checkTrue(test$g[[1]] == 'test')
+  RUnit::checkTrue(train$g[[1]] == 'train')
+  rm(list = c('train', 'test'))
+}
+
+test_unpack_into <- function() {
+  # named unpacking
+  # looks like assignment: DESTINATION = NAME_VALUE_USING
+  d <- data.frame(x = 1:2,
+                  g=c('test', 'train'),
+                  stringsAsFactors = FALSE)
+  into[train_set = train, test_set = test] <- split(d, d$g)
+  # train_set and test_set now correctly split
+  RUnit::checkTrue(test_set$g[[1]] == 'test')
+  RUnit::checkTrue(train_set$g[[1]] == 'train')
+  rm(list = c('train_set', 'test_set'))
+
+  # again with self in local environment
+  # named unpacking
+  # looks like assignment: DESTINATION = NAME_VALUE_USING
+  d <- data.frame(x = 1:2,
+                  g=c('test', 'train'),
+                  stringsAsFactors = FALSE)
+  into[train_set = train, test_set = test] <- split(d, d$g)
+  # train_set and test_set now correctly split
+  RUnit::checkTrue(test_set$g[[1]] == 'test')
+  RUnit::checkTrue(train_set$g[[1]] == 'train')
+  rm(list = c('train_set', 'test_set'))
+
+  # named unpacking NEWNAME = OLDNAME implicit form
+  # values are matched by name, not index
+  into[train, test] <- split(d, d$g)
+  RUnit::checkTrue(test$g[[1]] == 'test')
+  RUnit::checkTrue(train$g[[1]] == 'train')
+  rm(list = c('train', 'test'))
+
+  # function version
+  into(split(d, d$g), train, test)
+  RUnit::checkTrue(test$g[[1]] == 'test')
+  RUnit::checkTrue(train$g[[1]] == 'train')
+  rm(list = c('train', 'test'))
+
+  # pipe version
+  split(d, d$g) %.>% into(., train, test)
+  RUnit::checkTrue(test$g[[1]] == 'test')
+  RUnit::checkTrue(train$g[[1]] == 'train')
+  rm(list = c('train', 'test'))
+}
+
+
+test_unpack_to <- function() {
+  # named unpacking
+  # looks like assignment: DESTINATION = NAME_VALUE_USING
+  d <- data.frame(x = 1:2,
+                  g=c('test', 'train'),
+                  stringsAsFactors = FALSE)
+  to[train_set = train, test_set = test] <- split(d, d$g)
+  # train_set and test_set now correctly split
+  RUnit::checkTrue(test_set$g[[1]] == 'test')
+  RUnit::checkTrue(train_set$g[[1]] == 'train')
+  rm(list = c('train_set', 'test_set'))
+
+  # again with self in local environment
+  # named unpacking
+  # looks like assignment: DESTINATION = NAME_VALUE_USING
+  d <- data.frame(x = 1:2,
+                  g=c('test', 'train'),
+                  stringsAsFactors = FALSE)
+  to[train_set = train, test_set = test] <- split(d, d$g)
+  # train_set and test_set now correctly split
+  RUnit::checkTrue(test_set$g[[1]] == 'test')
+  RUnit::checkTrue(train_set$g[[1]] == 'train')
+  rm(list = c('train_set', 'test_set'))
+
+  # named unpacking NEWNAME = OLDNAME implicit form
+  # values are matched by name, not index
+  to[train, test] <- split(d, d$g)
+  RUnit::checkTrue(test$g[[1]] == 'test')
+  RUnit::checkTrue(train$g[[1]] == 'train')
+  rm(list = c('train', 'test'))
+
+  # function version
+  to(split(d, d$g), train, test)
+  RUnit::checkTrue(test$g[[1]] == 'test')
+  RUnit::checkTrue(train$g[[1]] == 'train')
+  rm(list = c('train', 'test'))
+
+  # pipe version
+  split(d, d$g) %.>% to(., train, test)
+  RUnit::checkTrue(test$g[[1]] == 'test')
+  RUnit::checkTrue(train$g[[1]] == 'train')
+  rm(list = c('train', 'test'))
 }
